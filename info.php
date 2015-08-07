4cf10a4f8b6ec068158452f7c313c6a997fa133d
Bin Zhang <30135878@qq.com>
2015-08-07 10:49:46 +0800

    测试。

--- info.php
+++ info.php
@@ -8,6 +8,7 @@ if ($ip == '112.124.98.9') {
 	$db = new MysqliDb ('localhost:3306', 'root', 'Dsh12345', 'trafficpolice');
 }
 
+var_dump($ip);
 $users = $db->get('user');
 var_dump($users);
 if ($db->count > 0) {
