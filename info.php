628a538170f3ab08e07a6df17878a3c2bcb1e953
Bin Zhang <30135878@qq.com>
2015-08-07 10:36:13 +0800

    测试数据库。

--- info.php
+++ info.php
@@ -1,6 +1,19 @@
 <?php
 
+require_once ('./submodules/php-mysqli-database-class/MysqliDb.php');
+
+$db = new MysqliDb ('localhost:3306', 'root', 'Dsh12345', 'trafficpolice');
 $ip = $_SERVER['SERVER_ADDR'];
-var_dump($ip);
+if ($ip == '112.124.98.9') {
+	$db = new MysqliDb ('localhost:3306', 'root', 'Dsh12345', 'trafficpolice');
+}
+
+$users = $db->get('user');
+var_dump($users);
+if ($db->count > 0) {
+    foreach ($users as $user) { 
+        print_r ($user);
+    }
+}
 
 ?>
