c95c2f80659e5f2a2205afede2a6004b5f5e6834
Bin Zhang <30135878@qq.com>
2015-08-24 12:25:52 +0800

    Log Get或者POST中的参数。

--- index.php
+++ index.php
@@ -5,10 +5,6 @@ require 'vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-// create a log channel
 $log = new Logger('name');
 $log->pushHandler(new StreamHandler('log/log', Logger::WARNING));
-
-// add records to the log
-$log->addWarning('Foo');
-$log->addError('Bar');
+$log->addWarning('_REQUEST:' . var_export($_REQUEST, TRUE));
