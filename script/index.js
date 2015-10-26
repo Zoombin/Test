98544bbba94e52534975d32af02c5b2477733b3d
Bin Zhang <30135878@qq.com>
2015-10-26 20:45:11 +0800

    淘宝样式。

---
+++ script/index.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+	$('.tutuul li a img').click(function(){
+		var src = $(this).attr('src')
+		$('.tanchuceng .tanchuimg').attr("src",src)
+		
+		$(".tanchuceng").show();
+		
+	})
+	
+	$('.tanchuceng .chacha').click(function(){
+		$('.tanchuceng').hide()
+	})
+})
