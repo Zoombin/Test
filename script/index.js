ef1041e46713eb77a10a0597d5adeb1f2b368981
Bin Zhang <30135878@qq.com>
2015-10-30 12:19:40 +0800

    删除无用文件。

--- script/index.js
+++ script/index.js
@@ -2,11 +2,11 @@
 	$('.tutuul li a img').click(function(){
 		var src = $(this).attr('src')
 		$('.tanchuceng .tanchuimg img').attr("src",src)
-		
+
 		$(".tanchuceng").show();
-		
+
 	})
-	
+
 	$('.tanchuceng .chacha').click(function(){
 		$('.tanchuceng').hide()
 	})
